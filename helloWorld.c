d494a3e9c929394f5f647b97c85c471681dd08e4
JitkasemC <jitkasem.c@gmail.com>
2022-01-02 14:10:11 +0700

    print multiple variables in one line

--- helloWorld.c
+++ helloWorld.c
@@ -2,5 +2,10 @@
 
 int main() {
     printf("Hello, Book")
+
+    char firstNameInitial = 'J';
+    char lastNameInitial = 'C';
+    int age = 28;
+    printf("First name initial is %c, last name initial is %c. Age is %d\n", firstNameInitial, lastNameInitial, age);
     return 0;
 }
